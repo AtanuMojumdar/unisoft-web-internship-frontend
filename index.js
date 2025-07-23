d93b7727cc0f4954d03a523f40182cd78005b793
Atanu Mojumdar <atanu192003mojumdar@gmail.com>
2025-07-23 18:02:03 +0530

    auth commit

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-console.log("Hello World!")
+const emailInput = document.getElementById("exampleFormControlInput1");
+const passInput = document.getElementById("inputPassword5");
+const submitbtn = document.getElementById("submitbtn");
+const loginSubmitBtn = document.getElementById("loginSubmitBtn");
+
+
+submitbtn?.addEventListener("click", () => {
+    fetch("http://localhost:8000/signup", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+            email: emailInput.value,
+            password: passInput.value
+        })
+    })
+        .then((value) => {
+            return value.json()
+        })
+        .then((val) => {
+            console.log(val)
+        })
+        .catch((err)=>{
+            console.log("Error Occured:")
+            console.log(err)
+        })
+})
+
+loginSubmitBtn?.addEventListener("click",()=>{
+    fetch("http://localhost:8000/login", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+            email: emailInput.value,
+            password: passInput.value
+        })
+    })
+        .then((value) => {
+            return value.json()
+        })
+        .then((val) => {
+            console.log(val)
+        })
+        .catch((err)=>{
+            console.log("Error Occured:")
+            console.log(err)
+        })
+})
